fb9c530b7d1fce3974aac92fc450e3286f1f87b7
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-08-10 22:23:44 -0300

    up

--- Cadastro-Cartas-Paises/main.c
+++ Cadastro-Cartas-Paises/main.c
@@ -3,8 +3,18 @@
 
 int main()
 {
-    int populacao, pontos_turi;
-    float area, pib;
-    printf("//////////////////////////////////////////////// \n           Cadastro de Cartas Países \n////////////////////////////////////////////////");
+    int populacao1, populacao2, pontos_turi1, pontos_turi2;
+    float area1, area2, pib1, pib2;
+    char pais1, pais2;
+    printf("//////////////////////////////////////////////// \n           Cadastro de Cartas Países \n//////////////////////////////////////////////// \n");
+    printf("Digite o nome do País 1: \n");
+    scanf(" %s ", &pais1);
+    getchar();
+    printf("Digite o nome do País 2: \n");
+    scanf(" %s ", &pais2);
+    getchar();
+    printf("Digite a área de %c: \n", pais1);
+    scanf(" %d ", area1);
+
     return 0;
 }
