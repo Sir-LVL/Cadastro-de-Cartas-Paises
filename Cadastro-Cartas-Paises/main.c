75736cb8f87888aabf4cbacab862ea4921475400
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-08-10 21:37:02 -0300

    add print

--- Cadastro-Cartas-Paises/main.c
+++ Cadastro-Cartas-Paises/main.c
@@ -3,6 +3,8 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int populacao, pontos_turi;
+    float area, pib;
+    printf("//////////////////////////////////////////////// \n           Cadastro de Cartas Países \n////////////////////////////////////////////////");
     return 0;
 }
