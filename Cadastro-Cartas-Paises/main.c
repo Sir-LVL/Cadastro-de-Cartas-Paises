2c9db545deed373efbcef1e8d8494674792d2e88
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-08-11 09:11:25 -0300

    fix_busg

--- Cadastro-Cartas-Paises/main.c
+++ Cadastro-Cartas-Paises/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     int populacao1, populacao2, pontos_turi1, pontos_turi2;
     float area1, area2, pib1, pib2;
-    char pais1, pais2;
+    char pais1[50], pais2[50];
     printf("//////////////////////////////////////////////// \n           Cadastro de Cartas Países \n//////////////////////////////////////////////// \n");
     printf("Digite o nome do País 1: \n");
-    scanf(" %s ", &pais1);
-    getchar();
+    fgets(pais1, 50, stdin);
+    pais1[strcspn(pais1, "\n")] = '\0';
     printf("Digite o nome do País 2: \n");
-    scanf(" %s ", &pais2);
-    getchar();
-    printf("Digite a área de %c: \n", pais1);
-    scanf(" %d ", area1);
+    fgets(pais2, 50, stdin);
+    pais2[strcspn(pais2, "\n")] = '\0';
+    printf("Digite a área de %s: \n", pais1);
+    scanf("%f", area1);
 
     return 0;
 }
