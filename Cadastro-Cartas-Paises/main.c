ffea557a4209dd80aaa44672912a85c232d47ef2
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-08-13 17:37:00 -0300

    update

--- Cadastro-Cartas-Paises/main.c
+++ Cadastro-Cartas-Paises/main.c
@@ -12,8 +12,8 @@ int main()
     printf("//////////////////////////////////////////////// \n           Cadastro de Cartas Países \n//////////////////////////////////////////////// \n");
 
     printf("Digite o nome do País 1: \n");
-    fgets(pais1, 50, stdin);
-    pais1[strcspn(pais1, "\n")] = '\0';
+    fgets(pais1, 50, stdin);// Ler o nome do país
+    pais1[strcspn(pais1, "\n")] = '\0';// retira o \n que fica armazenado junto com a variavel, assim corrigindo o erro do código ser 'pulado'
 
     printf("Digite a área de %s: \n", pais1);
     scanf("%f", &area1);
@@ -27,11 +27,18 @@ int main()
     printf("Quantos pontos turisticos tem %s? \n", pais1);
     scanf("%d", &pontos_turi1);
 
-    densidade_popu1 = (float) populacao1 / area1;
+    densidade_popu1 = (float) populacao1 / area1; //calculo da densidade populacional
 
-    pibcapi1 = pib1 / (float) populacao1;
+    pibcapi1 = pib1 / (float) populacao1; // Pib per Capita
+    densidade_inver1 = 1 / densidade_popu1; // densidade invertida
 
-    densidade_inver1 = 1 / densidade_popu1;
+    printf("Carta 1: %s \n", pais1);
+    printf("Área: %.2f Km² \n", area1);
+    printf("População: %d \n", populacao1);
+    printf("Densidade populacional: %.2f hab/Km² \n", densidade_popu1);
+    printf("Quantidade de Pontos Turísticos: %d \n", pontos_turi1);
+    printf("PIB: R$%.2f \n", pib1);
+    printf("PIB per Capita: R$%.2f \n", pibcapi1);
 
     getchar();
 
@@ -56,6 +63,14 @@ int main()
     pibcapi2 = pib2 / (float) populacao2;
 
     densidade_inver2 = 1 / densidade_popu2;
+
+    printf("Carta 2: %s \n", pais2);
+    printf("Área: %.2f Km² \n", area2);
+    printf("População: %d \n", populacao2);
+    printf("Densidade populacional: %.2f hab/Km² \n", densidade_popu2);
+    printf("Quantidade de Pontos Turísticos: %d \n", pontos_turi2);
+    printf("PIB: R$%.2f \n", pib2);
+    printf("PIB per Capita: R$%.2f \n", pibcapi2);
 
     super1 = area1 + densidade_popu1 + densidade_inver1 + pib1 + pibcapi1 + populacao1 + pontos_turi1;
     super2 = area2 + densidade_popu2 + densidade_inver2 + pib2 + pibcapi2 + populacao2 + pontos_turi2;
